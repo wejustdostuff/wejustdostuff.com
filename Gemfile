6db0693ec3fcc756b96d35a1d252ec1fa7deae51
franklin <franklin@weareinteractive.com>
2018-05-20 18:17:11 +0200

    feat: update to middleman 4

--- Gemfile
+++ Gemfile
@@ -1,18 +1,44 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem "middleman", "~> 3.3"
-gem "middleman-robots", "~> 1.0"
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+
+#gem "middleman-robots", "~> 1.0"
+#gem "middleman-livereload", "~> 3.4"
+#gem "middleman-google-analytics", "~> 1.1"
+#gem "middleman-search_engine_sitemap", "~> 1.3"
+
+#gem "middleman-deploy", "~> 1.0"
+#gem "middleman-imageoptim", "~> 0.1"
+#gem "middleman-minify-html", "~> 3.1"
+#gem "middleman-favicon-maker", "~> 3.7"
+
+#gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+#gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+# Added at 2018-05-20 14:30:48 +0200 by franklin:
 gem "middleman-livereload", "~> 3.4"
-gem "middleman-google-analytics", "~> 1.1"
-gem "middleman-search_engine_sitemap", "~> 1.3"
 
-gem "middleman-deploy", "~> 1.0"
-gem "middleman-imageoptim", "~> 0.1"
-gem "middleman-minify-html", "~> 3.1"
-gem "middleman-favicon-maker", "~> 3.7"
+# Added at 2018-05-20 14:33:11 +0200 by franklin:
+gem "middleman-google-analytics", "~> 3.0"
 
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+# Added at 2018-05-20 14:44:34 +0200 by franklin:
+gem "middleman-sprockets", "~> 4.1"
 
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+# Added at 2018-05-20 15:02:13 +0200 by franklin:
+gem "middleman-compass", "~> 4.0"
+
+# Added at 2018-05-20 15:07:16 +0200 by franklin:
+gem "middleman-robots", "~> 1.3"
+
+# Added at 2018-05-20 15:07:36 +0200 by franklin:
+gem "middleman-favicon-maker", "~> 4.1"
+
+# Added at 2018-05-20 15:07:57 +0200 by franklin:
+gem "middleman-minify-html", "~> 3.4"
+
+# Added at 2018-05-20 15:08:20 +0200 by franklin:
+gem "middleman-imageoptim", :git => "https://github.com/plasticine/middleman-imageoptim", :branch => "master"
+
+# Added at 2018-05-20 15:09:38 +0200 by franklin:
+gem "middleman-search_engine_sitemap", "~> 1.4"
--- config.rb
+++ config.rb
@@ -1,96 +1,110 @@
-###
-# Page options, layouts, aliases and proxies
-###
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-# Per-page layout changes:
-#
-# With no layout
-page "/404.html", :layout => false
+activate :sprockets
 
-###
-# Assets
-###
-
-set :js_dir, 'assets/js'
-set :css_dir, 'assets/css'
-set :images_dir, 'assets/img'
-set :fonts_dir, 'assets/fonts'
-
-after_configuration do
-  sprockets.append_path File.join "#{root}", "bower_components"
-end
-
-###
-# Extensions
-###
-
-set :url_root, data.site.url
 activate :search_engine_sitemap
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
 
 activate :google_analytics do |ga|
   ga.anonymize_ip = true
   ga.tracking_id = data.site.ga_account
 end
 
-# Development-specific configuration
-configure :development do
-	activate :livereload
-end
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.txt', layout: false
+page '/*.json', layout: false
+page "/404.html", layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Assets
+
+set :js_dir, 'assets/js'
+set :css_dir, 'assets/css'
+set :images_dir, 'assets/img'
+set :fonts_dir, 'assets/fonts'
+set :url_root, @app.data.site.url
+
+sprockets.append_path File.join "#{root}", "bower_components"
 
 # Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
 configure :build do
 	# robots.txt
 	activate :robots,
 		:rules => [
 	    {:user_agent => '*', :allow => %w(/)}
 		],
-		:sitemap => "#{data.site.url}/sitemap.xml"
+		:sitemap => "#{@app.data.site.url}/sitemap.xml"
   # cache
+  activate :imageoptim
   activate :minify_css
   activate :minify_html
   activate :cache_buster
   activate :minify_javascript
-  activate :imageoptim do |options|
-	  options.manifest = true
-    #options.pngout = false
-    #options.advpng = false
-	  options.skip_missing_workers = false
-  end
   # favicon
-  activate :favicon_maker, :icons => {
-    "favicon_base.png" => [
-	    { icon: "apple-touch-icon-152x152-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS7.
-	    { icon: "apple-touch-icon-144x144-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS6 or prior.
-	    { icon: "apple-touch-icon-120x120-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS7.
-	    { icon: "apple-touch-icon-114x114-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS6 or prior.
-	    { icon: "apple-touch-icon-76x76-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS7.
-	    { icon: "apple-touch-icon-72x72-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS6 or prior.
-	    { icon: "apple-touch-icon-60x60-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPhone with iOS7.
-	    { icon: "apple-touch-icon-57x57-precomposed.png" },               # iPhone and iPad users can turn web pages into icons on their home screen. Such link appears as a regular iOS native application. When this happens, the device looks for a specific picture. The 57x57 resolution is convenient for non-retina iPhone with iOS6 or prior. Learn more in Apple docs.
-	    { icon: "apple-touch-icon-precomposed.png", size: "57x57" },      # Same as apple-touch-icon.png, expect that is already have rounded corners (but neither drop shadow nor gloss effect).
-	    { icon: "apple-touch-icon.png", size: "57x57" },                  # Same as apple-touch-icon-57x57.png, for "default" requests, as some devices may look for this specific file. This picture may save some 404 errors in your HTTP logs. See Apple docs
-	    { icon: "favicon-196x196.png" },                                  # For Android Chrome M31+.
-	    { icon: "favicon-160x160.png" },                                  # For Opera Speed Dial (up to Opera 12; this icon is deprecated starting from Opera 15), although the optimal icon is not square but rather 256x160. If Opera is a major platform for you, you should create this icon yourself.
-	    { icon: "favicon-96x96.png" },                                    # For Google TV.
-	    { icon: "favicon-32x32.png" },                                    # For Safari on Mac OS.
-	    { icon: "favicon-16x16.png" },                                    # The classic favicon, displayed in the tabs.
-	    { icon: "favicon.png", size: "16x16" },                           # The classic favicon, displayed in the tabs.
-	    { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },         # Used by IE, and also by some other browsers if we are not careful.
-	    { icon: "mstile-70x70.png", size: "70x70" },                      # For Windows 8 / IE11.
-	    { icon: "mstile-144x144.png", size: "144x144" },
-	    { icon: "mstile-150x150.png", size: "150x150" },
-	    { icon: "mstile-310x310.png", size: "310x310" },
-	    { icon: "mstile-310x150.png", size: "310x150" }
-	  ]
-  }
+  activate :favicon_maker do |f| 
+    f.template_dir = "source/assets/img"
+    f.icons = {
+      "favicon.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS7.
+        { icon: "apple-touch-icon-144x144-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPad with iOS6 or prior.
+        { icon: "apple-touch-icon-120x120-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS7.
+        { icon: "apple-touch-icon-114x114-precomposed.png" },             # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS6 or prior.
+        { icon: "apple-touch-icon-76x76-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS7.
+        { icon: "apple-touch-icon-72x72-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS6 or prior.
+        { icon: "apple-touch-icon-60x60-precomposed.png" },               # Same as apple-touch-icon-57x57.png, for non-retina iPhone with iOS7.
+        { icon: "apple-touch-icon-57x57-precomposed.png" },               # iPhone and iPad users can turn web pages into icons on their home screen. Such link appears as a regular iOS native application. When this happens, the device looks for a specific picture. The 57x57 resolution is convenient for non-retina iPhone with iOS6 or prior. Learn more in Apple docs.
+        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },      # Same as apple-touch-icon.png, expect that is already have rounded corners (but neither drop shadow nor gloss effect).
+        { icon: "apple-touch-icon.png", size: "57x57" },                  # Same as apple-touch-icon-57x57.png, for "default" requests, as some devices may look for this specific file. This picture may save some 404 errors in your HTTP logs. See Apple docs
+        { icon: "favicon-196x196.png" },                                  # For Android Chrome M31+.
+        { icon: "favicon-160x160.png" },                                  # For Opera Speed Dial (up to Opera 12; this icon is deprecated starting from Opera 15), although the optimal icon is not square but rather 256x160. If Opera is a major platform for you, you should create this icon yourself.
+        { icon: "favicon-96x96.png" },                                    # For Google TV.
+        { icon: "favicon-32x32.png" },                                    # For Safari on Mac OS.
+        { icon: "favicon-16x16.png" },                                    # The classic favicon, displayed in the tabs.
+        { icon: "favicon.png", size: "16x16" },                           # The classic favicon, displayed in the tabs.
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },         # Used by IE, and also by some other browsers if we are not careful.
+        { icon: "mstile-70x70.png", size: "70x70" },                      # For Windows 8 / IE11.
+        { icon: "mstile-144x144.png", size: "144x144" },
+        { icon: "mstile-150x150.png", size: "150x150" },
+        { icon: "mstile-310x310.png", size: "310x310" },
+        { icon: "mstile-310x150.png", size: "310x150" }
+      ]
+    }
+  end
 end
 
-# Deploy-specific configuration
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.method = :rsync
-  deploy.clean  = true
-  deploy.host   = "www.wejustdostuff.com"
-  deploy.path   = "/var/www/wejustdostuff/htdocs"
-  deploy.flags  = "-avz --chmod=Dg+s,ug+w,+r"
+configure :development do
+	activate :livereload
 end
